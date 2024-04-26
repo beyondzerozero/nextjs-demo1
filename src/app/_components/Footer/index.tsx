82100b12f592e405c25f3cb80e7f7d3ef28db832
beyondzerozero <beyond-zero@naver.com>
2024-04-26 19:27:08 +0900

    페이지링크 오류

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -6,10 +6,10 @@ export function Footer() {
     <footer className={styles.footer}>
       <ul>
         <li>
-          <Link href="/privacy-policy">개인정보보호</Link>
+          <Link href="/privacy">개인정보보호</Link>
         </li>
         <li>
-          <Link href="/company-info">회사소개</Link>
+          <Link href="/company">회사소개</Link>
         </li>
       </ul>
     </footer>
